d217950554f1d61883d07898aa38b3e1bc2acade
Aman Tailor <amantailorpw@ip-10-212-128-38.ap-south-1.compute.internal>
2024-08-03 16:00:24 +0530

    props and tailwind

---
+++ tailwindpros02/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <>
+      <h1>React with tailwind</h1>
+      <Card userName='Aman' />
+      <Card post='SDE-1'/>
+      <Card />
+    </>
+  );
+}
+
+export default App;
